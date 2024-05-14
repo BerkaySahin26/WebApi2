9d6489644086c67300461f5e91b9f6a6ea2adc93
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2024-05-14 16:24:15 +0300

    Add project files.

---
+++ Business/Concretes/BrandManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstacts;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using DataAccess.Abstacts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class BrandManager:IBrandService
+    {
+       private readonly IBrandDal _brandDal;
+
+        public BrandManager(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+
+        public CreatedBrandResponse Add(CreateBrandRequest createBrandRequest)
+        {
+            //mapping
+            Brand brand = new();
+            brand.Name = createBrandRequest.Name;
+            brand.CreatedDate = DateTime.Now;
+            _brandDal.add(brand);
+
+            
+            //mapping
+            CreatedBrandResponse createBrandResponse = new CreatedBrandResponse();
+            createBrandResponse.Name = createBrandRequest.Name;
+            createBrandResponse.Id = 4;
+            createBrandResponse.CreatedDate = brand.CreatedDate;
+
+            return createBrandResponse;
+        }
+
+        public List<GetAllBrandResponse> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities;
+
+public class BaseEntity<TId>
+{
+    public TId Id { get; set; } // ıd tek tip olmak zorunda değil
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+    public DateTime? DaletedDate { get; set; }
+
+
+}
